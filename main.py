67236bfde8c50f25f794254cfff52b81e48705b6
Harry Dobbs <harrydobbs87@gmail.com>
2022-11-05 22:02:59 +1300

    ye

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+import selenium 
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import time 
+
+
+
+
+class TAB():
+
+  def __init__(self):
+    pass
+
+  def 
+
+
+
+def main():
